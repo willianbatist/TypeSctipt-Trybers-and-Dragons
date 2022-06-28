e3306184fddfbf59d7ad02ce75c667f8796b4dfd
Willian <willian.alves.b15@gmail.com>
2022-06-27 21:02:54 -0300

    req 02 completo

--- src/Races/index.ts
+++ src/Races/index.ts
@@ -1,6 +1,8 @@
 import Race from './Race';
 import Dwarf from './Dwarf';
 import Elf from './Elf';
+import Halfling from './Halfling';
+import Orc from './Orc';
 
-export { Dwarf, Elf };
+export { Dwarf, Elf, Halfling, Orc };
 export default Race;
